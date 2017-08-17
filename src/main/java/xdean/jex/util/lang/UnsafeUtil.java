8ab0d8adf4017ca42c2ce8782e251a288df931e7
XDean <373216024@qq.com>
2017-08-17 14:17:23 +0800

    access sun/** UnsafeUtil and test

--- src/main/java/xdean/jex/util/lang/UnsafeUtil.java
+++ src/main/java/xdean/jex/util/lang/UnsafeUtil.java
@@ -5,15 +5,148 @@ import java.security.AccessController;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 
+import org.slf4j.Logger;
+
 import sun.misc.Unsafe;
-import xdean.jex.extra.LazyValue;
+import xdean.jex.util.log.LogUtil;
+import xdean.jex.util.reflect.ReflectUtil;
 
 public class UnsafeUtil {
 
-  private static final LazyValue<Unsafe> THE_UNSAFE = LazyValue.create(() -> initUnsafe());;
+  private static final Unsafe THE_UNSAFE = initUnsafe();
+  private static final int ADDRESS_SIZE = THE_UNSAFE.addressSize();
+  private static final long HEADER_SIZE = initHeaderSize();
+  private static final boolean useCompressedOops = initUseCompressedOops();
+  private static final Logger LOGGER = LogUtil.log();
+  static {
+    LOGGER.debug("address size: {}", ADDRESS_SIZE);
+    LOGGER.debug("header size: {}", HEADER_SIZE);
+    LOGGER.debug("useCompressedOops: {}", useCompressedOops);
+  }
 
   public static Unsafe getUnsafe() {
-    return THE_UNSAFE.get();
+    return THE_UNSAFE;
+  }
+
+  /**
+   * Get the memory address of the given object.
+   *
+   * @param o
+   * @return
+   */
+  public static long addressOf(Object o) {
+    Object[] array = new Object[] { o };
+    long baseOffset = THE_UNSAFE.arrayBaseOffset(Object[].class);
+    long objectAddress;
+    switch (ADDRESS_SIZE) {
+    case 4:
+      objectAddress = THE_UNSAFE.getInt(array, baseOffset);
+      break;
+    case 8:
+      objectAddress = THE_UNSAFE.getLong(array, baseOffset);
+      break;
+    default:
+      throw new IllegalStateException("unsupported address size: " + ADDRESS_SIZE);
+    }
+    return objectAddress;
+  }
+
+  /**
+   * Measure the size of the given class.
+   *
+   * @param clz
+   * @return
+   */
+  public static long sizeOf(Class<?> clz) {
+    if (PrimitiveTypeUtil.isPrimitive(clz)) {
+      return PrimitiveTypeUtil.sizeOf(clz);
+    } else {
+      long maxOffset = 0L;
+      Field maxField = null;
+      for (Field field : ReflectUtil.getAllFields(clz, false)) {
+        long offset = THE_UNSAFE.objectFieldOffset(field);
+        if (offset > maxOffset) {
+          maxOffset = offset;
+          maxField = field;
+        }
+      }
+      if (maxOffset == 0) {
+        maxOffset = HEADER_SIZE;
+      } else if (maxOffset < HEADER_SIZE) {
+        throw new IllegalStateException("Can't calculate " + clz + "'s size.");
+      } else {
+        maxOffset += refSizeOf(maxField.getType());
+      }
+      return ((int) Math.ceil((double) maxOffset / ADDRESS_SIZE)) * ADDRESS_SIZE;
+    }
+  }
+
+  /**
+   * Get the size of reference.(considered UseCompressedOops)<br>
+   * If you want to get the actual address size, use {@link Unsafe#addressSize()}
+   *
+   * @param clz
+   * @return
+   */
+  public static long refSize() {
+    return useCompressedOops ? 4 : ADDRESS_SIZE;
+  }
+
+  /**
+   * Get the size of reference.(considered UseCompressedOops)<br>
+   * If the class is primitive type, return it's actual size.
+   *
+   * @param clz
+   * @return
+   */
+  public static long refSizeOf(Class<?> clz) {
+    if (PrimitiveTypeUtil.isPrimitive(clz)) {
+      return PrimitiveTypeUtil.sizeOf(clz);
+    } else {
+      return refSize();
+    }
+  }
+
+  public static long getHeaderSize() {
+    return HEADER_SIZE;
+  }
+
+  /**
+   * Determined whether the vm use -XX:+UseCompressedOops.
+   *
+   * @return If it's running on a 32 bit vm, it also return true;
+   */
+  public static boolean isUsecompressedOops() {
+    return useCompressedOops;
+  }
+
+  private static boolean initUseCompressedOops() {
+    @SuppressWarnings("unused")
+    class Helper {
+      Object a, b;
+    }
+    try {
+      Field a = Helper.class.getDeclaredField("a");
+      Field b = Helper.class.getDeclaredField("b");
+      long aOffset = THE_UNSAFE.objectFieldOffset(a);
+      long bOffset = THE_UNSAFE.objectFieldOffset(b);
+      return Math.abs(bOffset - aOffset) == 4;
+    } catch (NoSuchFieldException e) {
+      throw new Error("Never happen.", e);
+    }
+  }
+
+  private static long initHeaderSize() {
+    @SuppressWarnings("unused")
+    class Helper {
+      Object o;
+    }
+    Field[] fields = ReflectUtil.getAllFields(Helper.class, false);
+    if (fields.length != 1) {
+      throw new Error("Java Object don't behavior as expect. Check your java version or your code.");
+    }
+    Field f = fields[0];
+    return THE_UNSAFE.objectFieldOffset(f);
   }
 
   private static Unsafe initUnsafe() {
---
+++ src/test/java/xdean/jex/util/lang/TestUnsafeUtil.java
@@ -0,0 +1,66 @@
+package xdean.jex.util.lang;
+
+import static org.junit.Assert.*;
+import static xdean.jex.util.lang.UnsafeUtil.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TestUnsafeUtil {
+  @Test
+  public void testGetUnsafe() {
+    assertNotNull(getUnsafe());
+  }
+
+  @Ignore("not implement yet")
+  @Test
+  public void testAddressOf() {
+    System.out.printf("%s\n", Long.toHexString(addressOf(new Object())));
+    System.out.printf("%s\n", Long.toHexString(addressOf(new Object())));
+    System.out.printf("%s\n", Long.toHexString(addressOf(new Object())));
+  }
+
+  @Test
+  public void testSizeOf() {
+    if (System.getProperty("sun.arch.data.model").equals("64")) {
+      if (UnsafeUtil.isUsecompressedOops()) {
+        assertEquals(12, getHeaderSize());
+        assertEquals(16, sizeOf(Object.class));
+        assertEquals(32, sizeOf(SizeA.class));
+        assertEquals(32, sizeOf(SizeB.class));
+        assertEquals(24, sizeOf(SizeC.class));
+        assertEquals(24, sizeOf(SizeD.class));
+      } else {
+        assertEquals(16, getHeaderSize());
+        assertEquals(16, sizeOf(Object.class));
+        assertEquals(32, sizeOf(SizeA.class));
+        assertEquals(40, sizeOf(SizeB.class));
+        assertEquals(32, sizeOf(SizeC.class));
+        assertEquals(40, sizeOf(SizeD.class));
+      }
+    } else {
+      throw new UnsupportedOperationException("Haven't test ont 32 bit yet.");
+    }
+  }
+
+  static class SizeA {
+    byte a;
+    int b;
+    byte c;
+    long d;
+    byte e;
+  }
+
+  static class SizeB extends SizeA {
+    int f;
+  }
+
+  static class SizeC {
+    int a;
+    Object c;
+  }
+
+  static class SizeD {
+    Object a, b, c;
+  }
+}
