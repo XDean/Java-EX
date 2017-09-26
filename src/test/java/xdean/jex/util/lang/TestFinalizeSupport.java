652434c1e6ac6ba305b465f1e3f6220890761496
XDean <373216024@qq.com>
2017-09-26 10:30:56 +0800

    test FinalizeSupport

---
+++ src/test/java/xdean/jex/util/lang/TestFinalizeSupport.java
@@ -0,0 +1,23 @@
+package xdean.jex.util.lang;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Test;
+
+import xdean.jex.util.task.If;
+
+public class TestFinalizeSupport {
+  @Test
+  public void test() throws Exception {
+    AtomicBoolean called = new AtomicBoolean(false);
+    FinalizeSupport.finalize(new Object(),
+        () -> If.that(called.compareAndSet(false, true)).ordo(() -> fail("Finalize runnable called more than once!")));
+    System.gc();
+    Thread.sleep(10);
+    System.gc();
+    Thread.sleep(10);
+    assertTrue(called.get());
+  }
+}
