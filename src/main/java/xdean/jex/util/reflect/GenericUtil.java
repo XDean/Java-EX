f1da24e1ede064b5de37bdde5351988c5c289bea
XDean <373216024@qq.com>
2017-10-19 15:31:58 +0800

    use custom ParamerizedType

--- src/main/java/xdean/jex/util/reflect/GenericUtil.java
+++ src/main/java/xdean/jex/util/reflect/GenericUtil.java
@@ -15,14 +15,90 @@ import java.util.Map;
 import java.util.Objects;
 
 import lombok.extern.slf4j.Slf4j;
-import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
 @Slf4j
 public class GenericUtil {
   private static final Type[] EMPTY_TYPE_ARRAY = new Type[0];
 
   public static ParameterizedType createParameterizedType(Class<?> rawType, Type ownerType, Type... actualTypeArguments) {
-    return ParameterizedTypeImpl.make(rawType, actualTypeArguments, ownerType);
+    return new ParameterizedType() {
+      @Override
+      public Type getRawType() {
+        return rawType;
+      }
+
+      @Override
+      public Type getOwnerType() {
+        return ownerType;
+      }
+
+      @Override
+      public Type[] getActualTypeArguments() {
+        return actualTypeArguments;
+      }
+
+      @Override
+      public boolean equals(Object o) {
+        if (o instanceof ParameterizedType) {
+          ParameterizedType that = (ParameterizedType) o;
+          if (this == that) {
+            return true;
+          }
+          Type thatOwner = that.getOwnerType();
+          Type thatRawType = that.getRawType();
+          return (ownerType == null ? thatOwner == null : ownerType.equals(thatOwner)) &&
+              (rawType == null ? thatRawType == null : rawType.equals(thatRawType)) &&
+              Arrays.equals(actualTypeArguments, that.getActualTypeArguments());
+        } else {
+          return false;
+        }
+      }
+
+      @Override
+      public int hashCode() {
+        return Arrays.hashCode(actualTypeArguments) ^
+            (ownerType == null ? 0 : ownerType.hashCode()) ^
+            (rawType == null ? 0 : rawType.hashCode());
+      }
+
+      @Override
+      public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (ownerType != null) {
+          if (ownerType instanceof Class) {
+            sb.append(((Class<?>) ownerType).getName());
+          } else {
+            sb.append(ownerType.toString());
+          }
+          sb.append(".");
+          if (ownerType instanceof ParameterizedType) {
+            sb.append(rawType.getName().replace(
+                ((Class<?>) ((ParameterizedType) ownerType).getRawType()).getName() + "$", ""));
+          } else {
+            sb.append(rawType.getName());
+          }
+        } else {
+          sb.append(rawType.getName());
+        }
+        if (actualTypeArguments != null && actualTypeArguments.length > 0) {
+          sb.append("<");
+          boolean first = true;
+          for (Type t : actualTypeArguments) {
+            if (!first) {
+              sb.append(", ");
+            }
+            if (t instanceof Class) {
+              sb.append(((Class<?>) t).getName());
+            } else {
+              sb.append(t.toString());
+            }
+            first = false;
+          }
+          sb.append(">");
+        }
+        return sb.toString();
+      }
+    };
   }
 
   public static WildcardType createWildcardType(Type[] upperBounds, Type[] lowerBounds) {
