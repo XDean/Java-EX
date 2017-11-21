1bbc5016d1faed1e7be0b597d54060d3cbdd7b71
XDean <373216024@qq.com>
2017-11-21 15:23:29 +0800

    add ReferenceUtil

---
+++ src/test/java/xdean/jex/util/reflect/ReferenceUtilTest.java
@@ -0,0 +1,40 @@
+package xdean.jex.util.reflect;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import xdean.jex.util.lang.UnsafeUtil;
+
+public class ReferenceUtilTest {
+  @Test
+  public void testReference() throws Exception {
+    A a = new A();
+    a.b = new B();
+    a.b.c = Arrays.asList(new C());
+    a.b.c.get(0).d = new D[] { new D() };
+    assertTrue(ReferenceUtil.isReferencing(a, a.b.c.get(0).d[0]));
+    assertFalse(ReferenceUtil.isReferencing(a, new D()));
+    System.out.println(UnsafeUtil.sizeOf(Thread.currentThread()));
+    System.out.println(ReferenceUtil.isReferencing(Thread.currentThread(), a));
+  }
+
+  static class A {
+    B b;
+  }
+
+  static class B {
+    List<C> c;
+  }
+
+  static class C {
+    D[] d;
+  }
+
+  static class D {
+
+  }
+}
